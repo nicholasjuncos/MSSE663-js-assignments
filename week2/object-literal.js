b1ad7ee67a304579489853a7565dff14ce75f5a0
Nicholas Juncos <nicholasjuncos@Nicholass-MacBook-Pro.local>
2020-01-28 09:47:16 -0600

    Finished up object-literal section.

--- week2/object-literal.js
+++ week2/object-literal.js
@@ -6,3 +6,57 @@
  * #5 Convert the object literal to an Class
  * #6 Then call its method and output to the console.
  */
+
+let monkey = {
+    name: "Reggie",
+    age: 15,
+    gender: "M",
+    breed: "Orangutan",
+    getBreed: function() {
+        return this.breed;
+    },
+    getAgeString: function() {
+        return this.age.toString();
+    }
+};
+
+console.log(monkey);
+console.log(monkey.getBreed());
+console.log(monkey.getAgeString());
+
+class Monkey {
+    name;
+    age;
+    gender;
+    breed;
+
+    constructor(name, age, gender, breed) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.breed = breed;
+    }
+
+    getName() {
+        return this.name;
+    };
+    getAgeString() {
+        return this.age.toString();
+    };
+    getGender() {
+        if (this.gender.toUpperCase() === 'M') { return 'Male' }
+        if (this.gender.toUpperCase() === 'F') { return 'Female' }
+        return this.gender;
+    };
+    getBreed() {
+        return this.breed;
+    };
+}
+
+let testMonkey = new Monkey('James', 13, 'm', 'Chimp');
+console.log(Monkey);
+console.log(testMonkey);
+console.log(testMonkey.getName());
+console.log(testMonkey.getAgeString());
+console.log(testMonkey.getGender());
+console.log(testMonkey.getBreed());
