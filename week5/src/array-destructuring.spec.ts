27901c59bb9eb7804d15b1cc7b38d558b17d132a
Nicholas Juncos <nicholasjuncos@nicholass-mbp.attlocal.net>
2020-02-18 15:30:21 -0600

    Completed the array-destructing assignment.

--- week5/src/array-destructuring.spec.ts
+++ week5/src/array-destructuring.spec.ts
@@ -1,10 +1,20 @@
 import { expect } from 'chai';
 import 'mocha';
+import {a, b, fruits, c, d, others, food, statement} from "./array-destructuring";
 
 describe('Array destructuring tests', () => {
-  it('#1 should have a variable for every element in the given array', () => {});
+  it('#1 should have a variable for every element in the given array', () => {
+    expect(a).to.equal(fruits[1]);
+    expect(b).to.equal(fruits[0]);
+  });
 
-  it('#2 should have a variable for the first 2 elements then spread the rest', () => {});
+  it('#2 should have a variable for the first 2 elements then spread the rest', () => {
+    expect(c).to.equal(food[0]);
+    expect(d).to.equal(food[1]);
+    expect(others).to.eql(['chocolate', 'pears', 'oats', 'pizza']);
+  });
 
-  it('#3 should construct a statement from the given objarrayect', () => {});
+  it('#3 should construct a statement from the given objarrayect', () => {
+    expect(statement).to.equal("hello, taylor!")
+  });
 });
--- week5/src/array-destructuring.ts
+++ week5/src/array-destructuring.ts
@@ -2,10 +2,11 @@
 export const fruits = ['apple', 'banana'];
 
 // Currently, I would get the following:
-console.log(a); // 'apple'
-console.log(b); // 'banana'
+console.log(fruits[0]); // 'apple'
+console.log(fruits[1]); // 'banana'
 
 // deconstruct here
+export const [b, a] = fruits;
 
 // Assign variables using ES6 so that we get (note, you cannot just make a completely new array):
 console.log(a); // 'banana';
@@ -15,15 +16,16 @@ console.log(b); // 'apple';
 export const food = ['apple', 'banana', 'chocolate', 'pears', 'oats', 'pizza'];
 
 // deconstruct here
+export const [c, d, ...others] = food;
 
 // expected result:
-console.log(a); // 'apple'
-console.log(b); // 'banana'
+console.log(c); // 'apple'
+console.log(d); // 'banana'
 console.log(others); // ['chocolate, 'pears', 'oats', 'pizza']
 
-// #3 Array: Contruct a statement
-['hello', 'taylor'];
-
-// construct statement here
+// #3 Array: Construct a statement
+export const [hello, taylor] = ['hello', 'taylor'];
+export const statement = [hello, taylor].join(", ") + "!";
 
 // expected result: 'hello, taylor!'
+console.log(statement);
